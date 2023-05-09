91cadc5fefe8b395f87d0ecc5bcbaed8125a5fcf
Bhagyesh Rane <brane@adobe.com>
2023-05-09 14:42:12 +0530

    commit

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/CustomComponent.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/CustomComponent.java
@@ -4,16 +4,20 @@ import javax.annotation.PostConstruct;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 
 @Model(adaptables = { Resource.class,
         SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class CustomComponent {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomComponent.class);
 
     private String pageTitle;
 
@@ -21,18 +25,17 @@ public class CustomComponent {
     private String pagePath;
 
     @SlingObject
-    private PageManager pagemanager;
+    private ResourceResolver resourceResolver;
 
     @PostConstruct
     protected void init() {
         try {
             if (pagePath != null) {
-                Page page = pagemanager.getPage(pagePath);
-                pageTitle = page != null ? page.getTitle() != null ? page.getPageTitle() : "page title does not exist"
-                        : "page does not exist";
+                Page page = resourceResolver.getResource(pagePath).adaptTo(Page.class);
+                pageTitle = page != null ? page.getTitle() : null;
             }
         } catch (IllegalStateException e) {
-            pageTitle = "exception in sling model";
+            LOGGER.error("Exception in CustomComponent ", e);
         }
     }
 
